77768054ab034e30a82a889decb2a872dd6f5936
niftus13 <niftus13@gmail.com>
2023-03-24 15:02:45 +0900

    Update Prac14.java

--- java/Practice/src/Practice_Code/Prac14.java
+++ java/Practice/src/Practice_Code/Prac14.java
@@ -3,6 +3,7 @@ package Practice_Code;
 public class Prac14 {
 
 	public static void main(String[] args) {
+		// 10A+B + 10B+A = 99 => 11A+11B=99 =>A+B=9 => B-9=A
 		for (int a = 0; a <= 9; a++) {
 			for (int b = 0; b <= 9; b++) {
 				int top = a * 10 + b;
