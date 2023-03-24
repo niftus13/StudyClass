139e5e4195a89be6abf537eaaf2e25b8ee8b7da6
niftus13 <niftus13@gmail.com>
2023-03-24 15:05:02 +0900

    Update Prac13.java

--- java/Practice/src/Practice_Code/Prac13.java
+++ java/Practice/src/Practice_Code/Prac13.java
@@ -10,12 +10,10 @@ public class Prac13 {
 
 	public static void main(String[] args) {
 
-		for (int i = 2; i < 10; i += 2) {
-			for (int j = 1; j < 10; j++) {
+		for (int i = 2; i < 9; i *= 2) {
+			for (int j = 1; j <=i; j++) {
 				System.out.println(i + "X" + j + "=" + i * j + "\t");
-				if (j >= i || i == 6) {
-					break;
-				}
+
 			}
 		}
 
