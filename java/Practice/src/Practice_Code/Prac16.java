b0be5cce477c161c05399f51e788999ba66b014f
niftus13 <niftus13@gmail.com>
2023-03-24 09:39:39 +0900

    03-24
    daytime

--- java/Practice/src/Practice_Code/Prac16.java
+++ java/Practice/src/Practice_Code/Prac16.java
@@ -9,6 +9,30 @@ public class Prac16 {
 		int num = 0;
 		int sum = 0;
 		
+		while(num<100) {
+			sum+=num;
+			num++;
+		}
+		System.out.println("총 합은 "+sum);
+		num=0;
+		sum=0;
+		
+		do {
+			sum+=num;
+			num++;
+			
+		}while(num<100);
+		
+		System.out.println("총 합은 "+sum);
+		
+		
+		sum=0;
+		
+		for(num=0;num<100;num++) {
+			sum+=num;
+		}
+		System.out.println("총 합은 "+sum);
+		
 		
 
 	}
