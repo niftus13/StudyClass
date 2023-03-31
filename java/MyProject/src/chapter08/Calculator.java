835c49fe60b56390b1d5eb13fde413657c7f8433
niftus13 <niftus13@gmail.com>
2023-03-31 17:43:34 +0900

    0331

--- java/MyProject/src/chapter08/Calculator.java
+++ java/MyProject/src/chapter08/Calculator.java
@@ -4,6 +4,6 @@ public interface Calculator {
     long add(long num1, long num2);
     long sub(long num1, long num2);
     long multiply(long num1, long num2);
-    double divide(double num1, double num2) throws ArithmeticException;
+    double divide(double num1, double num2); 
 }
 
