76edad24fe186505c51ec1a1a2669f961e5ad071
niftus13 <niftus13@gmail.com>
2023-04-20 13:21:21 +0900

    0420

--- java/DeptManager/src/domain/Dept.java
+++ java/DeptManager/src/domain/Dept.java
@@ -8,6 +8,30 @@ public class Dept {
 	private String dname;
 	private String loc;
 	
+	public int getDeptno() {
+		return deptno;
+	}
+
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+
+	public String getDname() {
+		return dname;
+	}
+
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+
+	public String getLoc() {
+		return loc;
+	}
+
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+
 	//생성자
 	public Dept() {
 		
