886587eb7a09724e50641495c6572f2b2a96d2d2
niftus13 <niftus13@gmail.com>
2023-05-16 16:49:32 +0900

    0516

---
+++ springProject/intellij/DeptSpring/src/main/java/com/hi/deptspring/controller/DeptRegistController.java
@@ -0,0 +1,32 @@
+package com.hi.deptspring.controller;
+
+import com.hi.deptspring.domain.DeptRegistRequest;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j2
+@RequestMapping("/dept/regist")
+public class DeptRegistController {
+
+    @GetMapping
+    public String registForm(){
+        log.info("GET  /dept/regist");
+        return "/dept/registForm";
+    }
+    @PostMapping
+    public String regist(
+            DeptRegistRequest registRequest
+    ){
+        log.info("Post /dept/regist");
+        log.info(registRequest);
+
+        return "direct:/dept/list";
+    }
+
+
+
+}
---
+++ springProject/intellij/DeptSpring/src/main/webapp/WEB-INF/views/dept/registForm.jsp
@@ -0,0 +1,34 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hi
+  Date: 2023-05-16
+  Time: 오후 4:37
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<h1>부서등록</h1>
+<hr>
+<form method="post">
+    <table>
+        <tr>
+            <td>부서이름</td>
+            <td><input type="text" name="dname"></td>
+        </tr>
+        <tr>
+            <td>부서위치</td>
+            <td><input type="text" name="loc"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><input type="submit"></td>
+        </tr>
+    </table>
+</form>
+
+</body>
+</html>
