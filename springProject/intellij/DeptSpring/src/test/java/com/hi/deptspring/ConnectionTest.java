95472ff4ded1b99a1fb946e581aa3a5155b1abb5
niftus13 <niftus13@gmail.com>
2023-05-17 16:37:32 +0900

    0517

--- springProject/intellij/DeptSpring/src/test/java/com/hi/deptspring/ConnectionTest.java
+++ springProject/intellij/DeptSpring/src/test/java/com/hi/deptspring/ConnectionTest.java
@@ -1,6 +1,7 @@
 package com.hi.deptspring;
 
 import com.hi.deptspring.domain.DeptDTO;
+import com.hi.deptspring.domain.DeptSearchOption;
 import com.hi.deptspring.mapper.DeptMapper;
 import com.hi.deptspring.mapper.TimeMapper;
 import com.hi.deptspring.mapper.TimeMapper2;
@@ -15,6 +16,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Log4j2
@@ -34,9 +36,47 @@ public class ConnectionTest {
     private DeptMapper deptMapper;
 
     @Test
+    public void searchDeptTest() {
+        // 부서 이름검색
+        DeptSearchOption option1 = DeptSearchOption.builder().searchType("dname").keyword("S").build();
+        // 부서 위치검색
+        DeptSearchOption option2 = DeptSearchOption.builder().searchType("loc").keyword("NEW").build();
+        // 검색어가 없는 검색
+        DeptSearchOption option3 = new DeptSearchOption();
+
+        List<DeptDTO> list1 = deptMapper.selectByOption(option1);
+        List<DeptDTO> list2 = deptMapper.selectByOption(option2);
+        List<DeptDTO> list3 = deptMapper.selectByOption(option3);
+
+        log.info(list1);
+        log.info(list2);
+        log.info(list3);
+    }
+    @Test
+    public void selectByDeptnosTest(){
+        List<Integer> deptnos = new ArrayList<>();
+        deptnos.add(10);
+        deptnos.add(30);
+        deptnos.add(50);
+
+        List<DeptDTO> list = deptMapper.selectByDeptnos(deptnos);
+        log.info(list);
+    }
+
+    @Test
+    public void deptInsertTest() {
+        DeptDTO dept = DeptDTO.builder().dname("자바").loc("서울").deptno(10).build();
+
+        deptMapper.insertDept2(dept);
+
+    }
+
+
+    @Test
     public void getDeptListTest() {
 
-        List<DeptDTO> list = deptMapper.selectAll();
+//        List<DeptDTO> list = deptMapper.selectAll();
+        List<DeptDTO> list = deptMapper.selectAll2();
 
         log.info(">>>>>>> " + list);
 
@@ -56,6 +96,7 @@ public class ConnectionTest {
         log.info(timeMapper2.getTime());
 
     }
+
 
     @Test
     public void connectionTest() throws SQLException {
